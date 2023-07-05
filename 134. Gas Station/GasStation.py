b86917a8ea8013e180b65a74761971ac69bb389b
Xashqbb <mrhoriizonn@gmail.com>
2023-07-05 16:34:56 +0300

    134. Gas Station solution  for LeetCode

---
+++ 134. Gas Station/GasStation.py
@@ -0,0 +1,6 @@
+from Solution import Solution
+
+gas = [1,2,3,4,5]
+cost = [3,4,5,1,2]
+sol=Solution()
+print(sol.canCompleteCircuit(gas,cost))
---
+++ 134. Gas Station/Solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def canCompleteCircuit(self, gas, cost):
+        if sum(gas) < sum(cost):
+            return -1
+        total_tank = 0
+        current_tank = 0
+        start_station = 0
+        for i in range(len(gas)):
+            total_tank += gas[i] - cost[i]
+            current_tank += gas[i] - cost[i]
+            if current_tank < 0:
+                start_station = i + 1
+                current_tank = 0
+        return start_station
