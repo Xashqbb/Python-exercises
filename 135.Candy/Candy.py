4462638dcfa9029d08c773e2b6ca19e342f93b7d
Xashqbb <mrhoriizonn@gmail.com>
2023-07-13 16:10:26 +0300

    135. Candy solution  for LeetCode

---
+++ 135.Candy/Candy.py
@@ -0,0 +1,5 @@
+from Solution import Solution
+
+ratings = [1,0,2]
+sol=Solution()
+print(sol.candy(ratings))
---
+++ 135.Candy/Solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def candy(self, ratings):
+        n = len(ratings)
+        candies = [1] * n
+        # Traverse from left to right
+        for i in range(1, n):
+            if ratings[i] > ratings[i - 1]:
+                candies[i] = candies[i - 1] + 1
+        # Traverse from right to left
+        for i in range(n - 2, -1, -1):
+            if ratings[i] > ratings[i + 1] and candies[i] <= candies[i + 1]:
+                candies[i] = candies[i + 1] + 1
+
+        return sum(candies)
